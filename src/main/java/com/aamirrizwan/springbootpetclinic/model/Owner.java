8f74aa42e896fbea8e8dcb7b65e9f4de37226467
Aamir Rizwan <aamir.rizwan7@gmail.com>
2018-09-23 16:33:59 +0530

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/aamirrizwan/springbootpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.aamirrizwan.springbootpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/com/aamirrizwan/springbootpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.aamirrizwan.springbootpetclinic.model;
+
+public class Vet extends Person {
+
+}
